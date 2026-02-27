2377642e099a55842a8dde52d183ba268bb07eb4
Zainul342 <akuzainul176@gmail.com>
2026-02-27 13:18:36 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,14 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="id">
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=Inter:wght@300;400;500;600;700&display=swap"
+                    rel="stylesheet"
+                />
+            </head>
             <body>{children}</body>
         </html>
     );
